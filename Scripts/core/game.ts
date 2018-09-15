719e60d61dfd62a56127b604bb614746c9cbd856
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2018-09-15 18:52:55 -0400

    added Label class to the objects library

--- Scripts/core/game.ts
+++ Scripts/core/game.ts
@@ -3,7 +3,7 @@
     // game variables
     let canvas:HTMLCanvasElement;
     let stage:createjs.Stage;
-    let helloLabel:createjs.Text;
+    let helloLabel:objects.Label;
     let clickMeButton:createjs.Bitmap;
 
     function Start():void {
@@ -23,11 +23,7 @@
     }
 
     function Main():void {
-        helloLabel = new createjs.Text("Hello, World!", "60px Consolas", "#000000");
-        helloLabel.regX = helloLabel.getBounds().width * 0.5;
-        helloLabel.regY = helloLabel.getBounds().height * 0.5;
-        helloLabel.x = 320;
-        helloLabel.y = 240;
+        helloLabel = new objects.Label("Hello, World!","60px", "Consolas", "#00000",320,240, true);
         stage.addChild(helloLabel);
 
         clickMeButton = new createjs.Bitmap("/Assets/images/clickMeButton.png");
