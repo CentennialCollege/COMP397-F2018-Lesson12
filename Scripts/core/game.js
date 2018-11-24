5861b4e00fd83b0be78763b377d700764b8d35cc
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2018-11-24 18:12:04 -0500

    preloaded textureMap

--- Scripts/core/game.js
+++ Scripts/core/game.js
@@ -7,65 +7,10 @@
     var currentScene;
     var currentState;
     var scoreBoard;
-    var textureData = {
-        images: ["./Assets/sprites/texturemap.png"],
-        frames: [
-            [1, 1, 16, 16, 0, 0, 0],
-            [19, 1, 226, 178, 0, 0, 0],
-            [247, 1, 39, 40, 0, 0, 0],
-            [288, 1, 32, 40, 0, 0, 0],
-            [322, 1, 23, 40, 0, 0, 0],
-            [347, 1, 15, 40, 0, 0, 0],
-            [364, 1, 10, 40, 0, 0, 0],
-            [376, 1, 15, 40, 0, 0, 0],
-            [393, 1, 24, 40, 0, 0, 0],
-            [419, 1, 32, 40, 0, 0, 0],
-            [453, 1, 40, 40, 0, 0, 0],
-            [1, 181, 41, 40, 0, 0, 0],
-            [44, 181, 93, 74, 0, 0, 0],
-            [139, 181, 93, 74, 0, 0, 0],
-            [234, 181, 93, 74, 0, 0, 0],
-            [329, 181, 51, 52, 0, 0, 0],
-            [382, 181, 51, 52, 0, 0, 0],
-            [435, 181, 51, 52, 0, 0, 0],
-            [1, 257, 51, 52, 0, 0, 0],
-            [54, 257, 51, 52, 0, 0, 0],
-            [107, 257, 51, 52, 0, 0, 0],
-            [160, 257, 52, 50, 0, 0, 0],
-            [214, 257, 62, 62, 0, 0, 0],
-            [278, 257, 62, 51, 0, 0, 0],
-            [342, 257, 62, 51, 0, 0, 0],
-            [406, 257, 62, 51, 0, 0, 0],
-            [1, 321, 145, 47, 0, 0, 0],
-            [148, 321, 145, 47, 0, 0, 0]
-        ],
-        animations: {
-            bullet: { frames: [0] },
-            cloud: { frames: [1] },
-            coin: {
-                frames: [2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
-                speed: 1.0
-            },
-            enemy: {
-                frames: [12, 13, 14],
-                speed: 0.4
-            },
-            explosion: {
-                frames: [15, 16, 17, 18, 19, 20, 21],
-                speed: 1.0
-            },
-            island: { frames: [22] },
-            plane: {
-                frames: [23, 24, 25],
-                speed: 0.4
-            },
-            restartButton: { frames: [26] },
-            startButton: { frames: [27] }
-        }
-    };
     var textureMap;
     var assetManifest = [
         { id: "ocean", src: "./Assets/images/ocean.gif" },
+        { id: "textureMap", src: "./Assets/sprites/texturemap.png" },
         { id: "engineSound", src: "./Assets/audio/engine.ogg" },
         { id: "thunderSound", src: "./Assets/audio/thunder.ogg" },
         { id: "yaySound", src: "./Assets/audio/yay.ogg" },
@@ -87,6 +32,62 @@
         stage.enableMouseOver(20);
         createjs.Ticker.framerate = 60; // game will run at 60fps
         createjs.Ticker.on("tick", Update);
+        var textureData = {
+            images: [assetManager.getResult("textureMap")],
+            frames: [
+                [1, 1, 16, 16, 0, 0, 0],
+                [19, 1, 226, 178, 0, 0, 0],
+                [247, 1, 39, 40, 0, 0, 0],
+                [288, 1, 32, 40, 0, 0, 0],
+                [322, 1, 23, 40, 0, 0, 0],
+                [347, 1, 15, 40, 0, 0, 0],
+                [364, 1, 10, 40, 0, 0, 0],
+                [376, 1, 15, 40, 0, 0, 0],
+                [393, 1, 24, 40, 0, 0, 0],
+                [419, 1, 32, 40, 0, 0, 0],
+                [453, 1, 40, 40, 0, 0, 0],
+                [1, 181, 41, 40, 0, 0, 0],
+                [44, 181, 93, 74, 0, 0, 0],
+                [139, 181, 93, 74, 0, 0, 0],
+                [234, 181, 93, 74, 0, 0, 0],
+                [329, 181, 51, 52, 0, 0, 0],
+                [382, 181, 51, 52, 0, 0, 0],
+                [435, 181, 51, 52, 0, 0, 0],
+                [1, 257, 51, 52, 0, 0, 0],
+                [54, 257, 51, 52, 0, 0, 0],
+                [107, 257, 51, 52, 0, 0, 0],
+                [160, 257, 52, 50, 0, 0, 0],
+                [214, 257, 62, 62, 0, 0, 0],
+                [278, 257, 62, 51, 0, 0, 0],
+                [342, 257, 62, 51, 0, 0, 0],
+                [406, 257, 62, 51, 0, 0, 0],
+                [1, 321, 145, 47, 0, 0, 0],
+                [148, 321, 145, 47, 0, 0, 0]
+            ],
+            animations: {
+                bullet: { frames: [0] },
+                cloud: { frames: [1] },
+                coin: {
+                    frames: [2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+                    speed: 1.0
+                },
+                enemy: {
+                    frames: [12, 13, 14],
+                    speed: 0.4
+                },
+                explosion: {
+                    frames: [15, 16, 17, 18, 19, 20, 21],
+                    speed: 1.0
+                },
+                island: { frames: [22] },
+                plane: {
+                    frames: [23, 24, 25],
+                    speed: 0.4
+                },
+                restartButton: { frames: [26] },
+                startButton: { frames: [27] }
+            }
+        };
         // setup global spritesheet
         textureMap = new createjs.SpriteSheet(textureData);
         managers.Game.textureMap = textureMap;
